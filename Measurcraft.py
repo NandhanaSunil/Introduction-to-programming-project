b064a3f182c991df1a9e192057db5e0403fb6729
NandhanaSunil <133036831+NandhanaSunil@users.noreply.github.com>
2023-06-07 02:22:12 +0530

    Update Measurcraft.py

--- Measurcraft.py
+++ Measurcraft.py
@@ -219,7 +219,14 @@ class Ui_MainWindow(object):
         # To close the Window
         cap.release()
         cv2.destroyAllWindows()
-    
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())    
     
         
     
